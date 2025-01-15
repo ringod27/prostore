0b8dfb19fb64a77697cc95bbd399a4497c9dd6ac
Ringo Dulay <dulay.ringo.jp@gmail.com>
2025-01-16 00:35:04 +0900

    Test CICD

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -47,7 +47,7 @@ const ModeToggle = () => {
           checked={theme === "system"}
           onClick={() => setTheme("system")}
         >
-          System
+          System Test
         </DropdownMenuCheckboxItem>
         <DropdownMenuCheckboxItem
           checked={theme === "dark"}
