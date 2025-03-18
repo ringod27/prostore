1a6401d42c434bf6ff368311517d5890caf599b8
Ringo Dulay <dulay.ringo.jp@gmail.com>
2025-03-18 23:49:45 +0900

    add 09-order-history-and-user-profiles

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -10,6 +10,7 @@ import { prisma } from "@/db/prisma";
 import { revalidatePath } from "next/cache";
 import { paypal } from "../paypal";
 import { CartItem, PaymentResult } from "@/types";
+import { PAGE_SIZE } from "../constants";
 
 // Create Order
 export const createOrder = async () => {
@@ -258,4 +259,32 @@ async function updateOrderToPaid({
       throw new Error("Order not found");
     }
   });
+}
+
+// Get User Orders
+export async function getMyOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const session = await auth();
+  if (!session) throw new Error("User is not authenticated");
+
+  const data = await prisma.order.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.order.count({
+    where: { userId: session?.user?.id },
+  });
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
